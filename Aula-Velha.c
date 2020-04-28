175694c892c2260751ef3dc3332b024585c7ad81
Erica Lopes <33881813+ericaolopess@users.noreply.github.com>
2020-04-27 21:17:02 -0300

    Update Aula-Velha.c

--- Aula-Velha.c
+++ Aula-Velha.c
@@ -8,11 +8,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	int tabuleiro [LINHAS][COLUNAS];
+	char tabuleiro [LINHAS][COLUNAS];
 	int i, j;
 	int jogada, pos;
 	
-	
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<4; j++)
+		{
+			tabuleiro[i][j] = " ";
+		}
+	}
 	
 	printf ("___|___|___\n");
 	printf ("___|___|___\n");
@@ -26,6 +32,47 @@ int main(int argc, char *argv[]) {
 	
 	printf("Digite a posicao: ");
 	scanf("%d", &pos);
+	
+	switch (pos)
+	{
+		case 1:
+			tabuleiro[0][0] = "X";
+			break;
+		case 2:
+			tabuleiro[0][1] = "X";
+			break;
+		case 3:
+			tabuleiro[0][2] = "X";
+			break;
+
+		case 4:
+			tabuleiro[1][0] = "X";
+			break;
+		case 5:
+			tabuleiro[1][1] = "X";
+			break;
+		case 6:
+			tabuleiro[1][2] = "X";
+			break;
+			
+		case 7:
+			tabuleiro[2][0] = "X";
+			break;
+		case 8:
+			tabuleiro[2][1] = "X";
+			break;
+		case 9:
+			tabuleiro[2][2] = "X";
+			break;
+		default:
+			printf("Digite uma posição valida!!!");
+	}
+	
+	printf (" %d | %d | %d \n", tabuleiro[0][0],tabuleiro[0][1],tabuleiro[0][2]);
+	printf (" %d | %d | %d \n", tabuleiro[1][0],tabuleiro[1][1],tabuleiro[1][2]);
+	printf (" %d | %d | %d \n\n\n\n", tabuleiro[2][0],tabuleiro[2][1],tabuleiro[2][2]);
+	
+	
 	
 	/*recebe os dados
 	for(i=0; i<3; i++)
